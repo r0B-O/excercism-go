d8abf2d2830c23ad628abf3a4cc0cce58fa50f21
kiran-balachandran <kiran.balachandran@allianz.com>
2022-08-12 21:30:04 +0530

    iter-2: clean up

--- welcome-to-tech-palace/welcome_to_tech_palace.go
+++ welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -4,18 +4,21 @@ import "strings"
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
+	welMsg := "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 
-	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
+	return welMsg
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-
-	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
+	repStar := strings.Repeat("*", numStarsPerLine)
+	welMsg := repStar + "\n" + welcomeMsg + "\n" + repStar
+	return welMsg
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-
-	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
+	starRemoved := strings.ReplaceAll(oldMsg, "*", "")
+	trimmedMsg := strings.TrimSpace(starRemoved)
+	return trimmedMsg
 }
